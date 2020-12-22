d38a69626510f2511e7c048b2b68c204268983af
Группа 9 < 9@kvantorium.local>
2020-12-22 17:55:55 +0300

    Updated .performance design in performances.ejs.

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,6 +2,6 @@ selected = 0
 
 $(".gallery-image").on("click", function() {
     $(".gallery-preview").attr("src", $(this).attr("src"));
-    $(".gallery-selected").removeClass("selected");
+    $(".gallery-image.selected").removeClass("selected");
     $(this).addClass("selected");
 });
